cade06b60f4a740e35b33a4658aa054279f519d7
LilyCole <Lilia.Cole@gmail.com>
2016-10-08 16:22:31 -0700

    adds random colors to books

--- public/scripts/controllers/BooksIndexController.js
+++ public/scripts/controllers/BooksIndexController.js
@@ -17,13 +17,23 @@ function BooksIndexController ($http, $window) {
     .get('https://super-crud.herokuapp.com/books')
     .then(function(response) {
       vm.books = response.data.books;
+      vm.books.forEach(function(value,index) {
+        var color = randomColorTest(index);
+        value.style = {'background-color': color};
+      });
+      
     });
 
   vm.createBook = function() {
     $http
       .post('https://super-crud.herokuapp.com/books',vm.newBook)
       .then(function(response) {
-        vm.books.push(response.data);
+        var newBook = response.data;
+        index = vm.books.length;
+        var color = randomColorTest(index);
+        newBook.style = {'background-color': color};    
+        vm.books.push(newBook);
+        // vm.books.push(response.data);
       });
   }
 
@@ -39,4 +49,22 @@ function BooksIndexController ($http, $window) {
   vm.bookLink = function(bookId) {
     $window.location.href = "/books/"+bookId;
   }
+
+  var randomColor = function() {
+    var safeColors = ['00','33','66','99','cc','ff'];
+    // var safeColors = ['00','11','22','33','44','55'];
+    var r = safeColors[Math.floor(Math.random()*6)];
+    var g = safeColors[Math.floor(Math.random()*6)];
+    var b = safeColors[Math.floor(Math.random()*6)];
+    return "#"+r+g+b;
+  }
+
+  var randomColorTest = function(index) {
+    index = index % 17;
+    var safeColors = ['#F49AC2','#F49AC2','#C23B22','#FFD1DC','#966FD6',
+    '#AEC6CF','#77DD77','#CFCFC4','#B39EB5','#FFB347','#B19CD9','#FF6961',
+    '#03C03C','#FDFD96','#836953','#779ECB'];
+    return safeColors[index];
+  }
+
 }
